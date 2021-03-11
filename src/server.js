e02d73595eb2bb263103822f2d2fc5f00b7b1740
ouhouh1936 <ouhouh1936@gmail.com>
2021-03-11 20:18:53 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const PORT = 7000;
+const app = express();
+
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "/assets")));
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} SERVER START`);
+});
